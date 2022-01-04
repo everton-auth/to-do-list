522db78fff6325c5494de6083176560567086fa9
Everton-Auth <evertonauth@gmail.com>
2022-01-03 22:51:04 -0300

    copiando o whatsapp web na cara dura

--- IChat/src/app/modules/core/chat/chat.component.ts
+++ IChat/src/app/modules/core/chat/chat.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-  chat: any;
+  chat ='aa';
   constructor() { }
 
   ngOnInit() {
